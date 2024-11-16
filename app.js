a7f7767f547d07d1923a01c768e9557eee81f5a6
rayhanadri <rayerzz99@gmail.com>
2024-11-16 12:14:46 +0700

    enhancement

--- app.js
+++ app.js
@@ -1,15 +1,29 @@
-//Core module
-//file system
+const yargs = require("yargs");
 const contacts = require('./contacts');
 
-const main = async () => {
-    // console.log(contacts);
-    const nama = await contacts.tulisPertanyaan('Masukkan nama anda : ');
-    const email = await contacts.tulisPertanyaan('Masukkan email anda : ');
-    const noHP = await contacts.tulisPertanyaan('Masukkan noHP anda : ');
-    // const email = await pertanyaan2();
+yargs.command({
+    command : 'add',
+    describe : 'Menambahkan contact baru',
+    builder : {
+        nama: {
+            describe: "Nama lengkap",
+            demandOption: true,
+            type: 'string',
+        },
+        email: {
+            describe: 'Email',
+            demandOption: false,
+            type: 'string',
+        },
+        noHP: {
+            describe: 'Nomor Handphone',
+            demandOption: true,
+            type : 'string',
+        }
+    },
+    handler(argv){
+        contacts.simpanContact(argv.nama, argv.email, argv.noHP);
+    }
+});
 
-    contacts.simpanContact (nama, email, noHP);
-};
-
-main();
+yargs.parse();
--- contacts.js
+++ contacts.js
@@ -1,13 +1,16 @@
 
 const fs = require('fs');
+const chalk = require('chalk');
+const validator = require('validator');
+
 const { resolve } = require('path');
 
-const readline = require('readline');
+// const readline = require('readline');
 
-const rl = readline.createInterface({
-    input : process.stdin,
-    output : process.stdout,
-})
+// const rl = readline.createInterface({
+//     input : process.stdin,
+//     output : process.stdout,
+// })
 
 
 const dirPath = './data';
@@ -21,19 +24,39 @@ if(!fs.existsSync(dataPath)){
     fs.writeFileSync(dataPath,'[]', 'utf-8');
 }
 
-const tulisPertanyaan = (pertanyaan) => {
-    return new Promise((resolve, reject) => {
-        rl.question(pertanyaan, (nama) => {
-            resolve(nama);
-        });
-    })
-};
-
 const simpanContact = (nama, email, noHP) => {
     const contact = {nama, email, noHP};
     // console.log(contact);
     const fileBuffer = fs.readFileSync('data/contacts.json', 'utf-8')
     const contacts = JSON.parse(fileBuffer);
+
+    // cek duplikat
+    const duplikat = contacts.find((contact) => contact.nama === nama);
+    if (duplikat) {
+        console.log(
+            chalk.red.inverse.bold('Contact sudah terdaftar, gunakan nama lain!')
+        );
+        return false;
+    }
+
+    //cek email
+    if (email) {
+        if(!validator.isEmail(email)){
+            console.log(
+                chalk.red.inverse.bold('Email tidak valid!')
+            );
+            return false;
+        }
+    }
+
+    //no hp
+    if(!validator.isMobilePhone(noHP, 'id-ID')){
+        console.log(
+            chalk.red.inverse.bold('Nomor HP tidak valid!')
+        );
+        return false;
+    }
+
 
     contacts.push(contact);
 
@@ -42,9 +65,10 @@ const simpanContact = (nama, email, noHP) => {
 
     fs.writeFileSync('data/contacts.json', JSON.stringify(contacts));
         
-    console.log('terima kasih');
-    rl.close();
+    console.log(
+         chalk.green.inverse.bold('Terima kasih')
+    );
 }
 
-module.exports = {tulisPertanyaan, simpanContact};
+module.exports = {simpanContact};
 
